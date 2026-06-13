ee16bb2b4aa9f396a907593c8a73904c8782aa18
Hybridkernel <admin@hzqki.me>
2026-06-14 00:53:56 +1000

    [PRE] Feat: Brightblend, LUT, GPU picking for Rife, and parity changes

--- portable/assets/scripts/frameblend.py
+++ portable/assets/scripts/frameblend.py
@@ -5,10 +5,23 @@ import math
 import havsfunc
 
 def Frameblend(clip, args, recipe) -> vs.VideoNode:
+    bright_blend = recipe["frame_blending"]["bright_blend"]
+    
+    if bright_blend:
+        og_format = clip.format
+        try:
+            og_matrix = clip.get_frame(0).props['_Matrix']
+        except KeyError:
+            og_matrix = 1
+        clip = core.resize.Bicubic(clip=clip, format=vs.RGB48, transfer_in_s="709", transfer_s="linear", matrix_in_s="709")
+
     try:
         clip = core.frameblender.FrameBlend(clip, args["weighting"], True)
     except Exception as e:
         raise
+
+    if bright_blend:
+        clip = core.resize.Bicubic(clip=clip, format=og_format, matrix=og_matrix, transfer_s="709", transfer_in_s="linear", matrix_s="709")
 
     try:
         clip = havsfunc.ChangeFPS(
--- recipe/recipe.go
+++ recipe/recipe.go
@@ -1,12 +1,35 @@
 package recipe
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/smoothie-go/smoothie-go/cli"
+	"github.com/smoothie-go/smoothie-go/fruits"
 	"github.com/smoothie-go/smoothie-go/portable"
 	"gopkg.in/ini.v1"
 )
+
+func GetOutputFps(args *cli.Arguments, rc *Recipe) int {
+	if rc.FrameBlending.Enabled {
+		return rc.FrameBlending.Fps
+	}
+	if rc.Interpolation.Enabled {
+		return rc.Interpolation.Fps
+	}
+	if rc.PreInterp.Enabled {
+		var factor int
+		fmt.Sscanf(rc.PreInterp.Factor, "%d", &factor)
+		if factor <= 0 {
+			factor = 1
+		}
+		return args.InputFps * factor
+	}
+	return args.InputFps
+}
 
 func Parse(args *cli.Arguments) *Recipe {
 	defaults, err := ini.Load(portable.GetDefaultRecipePath())
@@ -38,6 +61,35 @@ func Parse(args *cli.Arguments) *Recipe {
 	recipe.ini_config = config
 
 	ParseDynamicMasking(recipe, args)
+
+	if !args.UserSpecifiedOutDir && recipe.Miscellaneous.GlobalOutputFolder != "" {
+		args.OutDir = recipe.Miscellaneous.GlobalOutputFolder
+		if _, err := os.Stat(args.OutDir); os.IsNotExist(err) {
+			log.Printf("WARNING: Global output folder %s does not exist. Falling back to default output directory.\n", args.OutDir)
+			args.OutDir = filepath.Dir(args.InputFile)
+		}
+	}
+
+	if !args.UserSpecifiedOutput && recipe.Output.FileFormat != "" {
+		inputBaseName := filepath.Base(args.InputFile)
+		extIndex := strings.LastIndex(inputBaseName, ".")
+		filename := inputBaseName
+		if extIndex != -1 {
+			filename = inputBaseName[:extIndex]
+		}
+
+		formatted := recipe.Output.FileFormat
+		formatted = strings.ReplaceAll(formatted, "%FILENAME%", filename)
+		formatted = strings.ReplaceAll(formatted, "%FRUIT%", fruits.GetRandomFruit())
+
+		outFps := GetOutputFps(args, recipe)
+		formatted = strings.ReplaceAll(formatted, "%OUTPUT_FPS%", fmt.Sprintf("%d", outFps))
+
+		formatted = strings.ReplaceAll(formatted, "%SPEED%", recipe.Interpolation.Speed)
+		formatted = strings.ReplaceAll(formatted, "%TUNING%", recipe.Interpolation.Tuning)
+
+		args.OutputFile = formatted
+	}
 
 	recipe = Validate(args, recipe)
 
